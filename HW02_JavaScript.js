772f9797853435609b8ad781bd65a35e97365d38
Sarah079 <u19032618@tuks.co.za>
2022-05-04 12:15:51 +0200

    Add modal

--- HW02_JavaScript.js
+++ HW02_JavaScript.js
@@ -12,12 +12,13 @@ $("document").ready(function () {
     
 //for each element in data array
     $.each(movies, function(){
-        var AddedData = `<div class='col-sm-3'>` +
-        `<div class='card' style='width: 18rem;'>`+
-           `<img class='card-img-top' src='`+ movieimage +`' alt='Card image cap'>` +
-           `<div class='card-body'>` 
-               "<h5 class='card-title'>"+ movieTitle +`</h5>` + //insert movie tartle 
+        var AddedData = `<div class='col-sm-3'>
+        <div class='card' style='width: 18rem;'>
+           <img class='card-img-top' src='`+ movieimage.toString +`' alt='Card image cap'>` +
+           `<div class='card-body'>
+               <h5 class='card-title'>"`+ movieTitle +`</h5>` + //insert movie tartle 
                 `<p class='card-text'>`+ movieDescrip +`</p>` + //insert description
+                `<p class='card-text float-right'>`+ moviePrice +`</p>` + //insert price
                 `<a class='btn btn-primary' id='showDetails'>Show Details</a>`  +
                 `<a class='btn btn-primary' id='BookTicket'>Book Ticket</a>`  +
             `</div>` +
@@ -25,11 +26,18 @@ $("document").ready(function () {
             `</div>`;
 
     $("#moviecards").append(AddedData);
+
     });
 
 //click show details
-    $("#showDetails").click(function(){
-        var detailsshow = `` 
+    $("#showDetails").click(function(e){
+        $(this).closest("#thisTitle").append(movieTitle);
+        $(this).closest("#thisDirector").append(movieDirector);
+        $(this).closest("#thisReleaseYear").append(movieReleaseyear);
+        $(this).closest("#thisRunTime").append(movieRuntime);
+
+        $("#myModal").modal('show');
+        console.log(e);
     });
 
 //cart table empty defualt
